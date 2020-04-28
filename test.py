ce424d23bb1bb09a5a2deba561f6d21a58a73881
Sheng <shengfang.ipeace@gmail.com>
2020-04-28 13:49:22 +0200

    add yolo_network.py

---
+++ test.py
@@ -0,0 +1,19 @@
+import tensorflow as tf
+
+from core import yolo_network
+
+
+INPUT_SIZE = (416, 416, 3)
+NB_CLASS = 20
+NB_ANCHOR = 3
+
+
+# darknet = yolo_network.build_darknet53(INPUT_SIZE, NB_CLASS)
+# darknet.summary()
+#
+# tf.keras.utils.plot_model(darknet, show_shapes=True)
+
+
+yolo3 = yolo_network.build_yolo_network(INPUT_SIZE, NB_CLASS, NB_ANCHOR)
+yolo3.summary()
+tf.keras.utils.plot_model(yolo3, show_shapes=True)
